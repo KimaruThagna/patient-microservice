5fec009743e6d46d84a8c4f06c9edd4bfa3771f6
Kimaru thagana <macbuntu@localhost.localdomain>
2020-08-12 13:59:42 +0300

    add federated object type

---
+++ patient/resolvers/federation/patients.py
@@ -0,0 +1,37 @@
+from ariadne.contrib.federation import FederatedObjectType
+
+from patient.models import Patient
+
+patient_federated_object = FederatedObjectType("Patient")
+
+
+@patient_federated_object.resolve_reference
+def get_patient_by_uid(representation):
+    return Patient.objects.get(uid=representation.get("uid"))
+
+
+@patient_federated_object.field("uid")
+def resolve_id(obj, *_):
+    return obj.uid
+
+
+@patient_federated_object.field("indexingId")
+def resolve_indexing_id(obj, *_):
+    return obj.indexing_id
+
+
+@patient_federated_object.field("first_name")
+def resolve_first_name(obj, *_):
+    return obj.first_name
+
+@patient_federated_object.field("last_name")
+def resolve_last_name(obj, *_):
+    return obj.last_name
+
+@patient_federated_object.field("license_number")
+def resolve_license_number(obj, *_):
+    return obj.patient_number
+
+@patient_federated_object.field("county")
+def resolve_county(obj, *_):
+    return obj.county
